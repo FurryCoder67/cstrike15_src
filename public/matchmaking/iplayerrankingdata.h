ffc35e1a8177a575eebf372dc2833b403d29c362
Aiden <aiden@rivi.us>
2026-07-17 18:17:10 -0400

    a

--- public/matchmaking/iplayerrankingdata.h
+++
@@ -1,9 +0,0 @@
-//========= Copyright © 1996-2011, Valve Corporation, All rights reserved. ============//
-//
-// Purpose:  Holds ELO data on Xbox
-//
-//=====================================================================================//
-#ifndef _IPLAYERRANKINGDATA_H_
-#define _IPLAYERRANKINGDATA_H_
-
-#endif  // _IPLAYERRANKINGDATA_H_
